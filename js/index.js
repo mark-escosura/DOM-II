5ff130fb9ac8d4c9e3803826487832a73204746a
mark-escosura <escosuramarkse@gmail.com>
2021-09-28 16:31:19 -0700

    Added Three event listeners so far and used preventDefault() to keep the navigation links from loading

--- js/index.js
+++ js/index.js
@@ -1,1 +1,48 @@
 // Your code goes here
+// ****** header ******
+
+// heading 
+const headerHeading = document.querySelector('.logo-heading');
+    
+
+headerHeading.addEventListener('mouseover', (event) =>{
+    event.target.style.color = 'blue';
+    // setTimeout(function(){
+    //     event.target.style.color = '';
+    // }, 1000);
+}, false);
+headerHeading.addEventListener('mouseout', (event) => {
+    event.target.style.color = '';
+},false);
+
+// nav 
+const nav = document.querySelector('header nav');
+
+nav.addEventListener('click', (event) =>{
+    event.preventDefault();
+});
+
+nav.addEventListener('mouseover', (event) =>{
+    event.target.style.color = 'green';
+    // setTimeout(function(event) { // determines how long the event will last
+    //     event.target.style.color = '';
+    // }, 1000);
+}, false);
+nav.addEventListener('mouseout', (event) =>{
+    event.target.style.color = '';
+}, false);
+
+
+
+// Images
+const images = document.querySelector('.home .intro img');
+
+images.addEventListener('dblclick', () => {
+    
+});
+console.log(images);
+// Welcome Sign
+
+
+
+
